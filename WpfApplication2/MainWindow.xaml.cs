5af3002a66d6f5a9c1a3806885374a543d0266e4
pomkalk <pomkalk@gmail.com>
2016-07-01 14:48:58 +0700

    snapshot

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Xml.Linq;
+
+
 namespace WpfApplication2
 {
     /// <summary>
@@ -23,6 +26,44 @@ namespace WpfApplication2
         public MainWindow()
         {
             InitializeComponent();
+            XDocument doc = XDocument.Load("a.xml");
+
+            load_level(doc.Root, this.treeView.Items);
         }
+
+        private string get_attributes(XElement el)
+        {
+            List<string> values = new List<string>();
+            foreach (XAttribute at in el.Attributes())
+            {
+                values.Add(at.Name + "=\"" + at.Value + "\"");
+            }
+
+            if (values.Count == 0)
+                return "";
+            return "  ["+String.Join(", ",values)+"]";
+        }
+
+        private void load_level(XElement root, ItemCollection obj)
+        {
+            foreach (XElement el in root.Elements())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = el.Name.ToString()+this.get_attributes(el);
+
+                item.MouseDoubleClick += item_MouseDoubleClick;
+
+                obj.Add(item);
+
+                load_level(el, item.Items);
+            }
+        }
+
+        void item_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            MessageBox.Show(item.Header.ToString());
+        }
+
     }
 }
